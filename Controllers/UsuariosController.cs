37ede9e24412995f8c58e4382f4c66aeaea1ba7f
Sergio David Rincon Murcia <sergiorincon50@gmail.com>
2022-08-27 22:22:09 -0500

    Documnetar API
    Se hace el modelo para documentar APIs en .NET Core 6

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -6,17 +6,42 @@ using WebApiExample.Models;
 
 namespace WebApiExample.Controllers
 {
+    /// <summary>
+    /// Controlador Usuarios
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class UsuariosController : ControllerBase
     {
         private readonly TestDataBaseContext _context;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
         public UsuariosController(TestDataBaseContext context)
         {
             _context = context;
         }
 
+        // Documentacion APIs
+        // https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages-using-swagger?view=aspnetcore-6.0
+        /// <summary>
+        /// Get Usuarios por ID
+        /// </summary>
+        /// <remarks>
+        /// Sample Request:
+        ///     POST
+        ///     
+        ///     {
+        ///         "date": "2022-08-28T03:12:01.110Z",
+        ///         "temperatureC": 0,
+        ///         "temperatureF": 0,
+        ///         "summary": "string"
+        ///     }
+        /// </remarks>
+        /// <response code = "200"> Bien </response>
+        /// <response code = "400"> Mal </response>
         [HttpGet]
         [Route("/GetUsiarios")]
         public async Task<IActionResult> GetUsuarios(string? id)
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using WebApiExample.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,12 @@ builder.Services.AddDbContext<TestDataBaseContext>(opt => opt.UseSqlServer(build
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
@@ -17,7 +23,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        // Desavilidar Try it out para Produccion
+        //c.SupportedSubmitMethods();
+    });
 }
 
 app.UseAuthorization();
