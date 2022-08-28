8cd79120aa2294d3c26904d744840e3ed3165fe0
Sergio David Rincon Murcia <sergiorincon50@gmail.com>
2022-08-27 21:30:57 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiExample.Negocio;
+using WebApiExample.Data;
+using WebApiExample.Models;
+
+namespace WebApiExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly TestDataBaseContext _context;
+
+        public UsuariosController(TestDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("/GetUsiarios")]
+        public async Task<IActionResult> GetUsuarios(string? id)
+        {
+            try
+            {
+                ClsUsuarios clsUsuarios = new ClsUsuarios(_context);
+                var list = await clsUsuarios.GetUsuarios(id);
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ Negocio/ClsUsuarios.cs
@@ -0,0 +1,22 @@
+﻿using WebApiExample.Data;
+using WebApiExample.Models;
+
+namespace WebApiExample.Negocio
+{
+    public class ClsUsuarios
+    {
+        private readonly TestDataBaseContext _context;
+
+        public ClsUsuarios(TestDataBaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<SpGetUsuariosResult>> GetUsuarios(string? IdUsuario)
+        {
+            IdUsuario = IdUsuario == null ? null : IdUsuario.ToString();
+            List<SpGetUsuariosResult> ListUsuarios = await _context.GetProcedures().SpGetUsuariosAsync(IdUsuario);
+            return ListUsuarios;
+        }
+        
+    }
+}
